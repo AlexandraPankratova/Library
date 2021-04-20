a85c5c8f1432728c296624e71ba22b4003774ea7
AlexandraPankratova <ablackcat@yandex.ru>
2021-04-20 11:30:23 +0300

    Create def download_image

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import requests
 
 
-def main():
-    url = "https://dvmn.org/filer/canonical/1542890876/16/"
-    response = requests.get(url)
+def download_image(image_url, image_name):
+    response = requests.get(image_url)
     response.raise_for_status()
 
-    filename = "dvmn.svg"
-    with open(filename, "wb") as file:
+    with open(image_name, "wb") as file:
         file.write(response.content)
+
+
+def main():
+    download_image(
+        "https://dvmn.org/filer/canonical/1542890876/16/",
+        "./images/dvmn.svg",
+    )
 
 
 if __name__ == '__main__':
