5d2fde912b4fe63de6deaaaf95cfb770c85a93ca
AlexandraPankratova <ablackcat@yandex.ru>
2021-04-20 13:52:15 +0300

    Upload directories from .env

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
+from dotenv import load_dotenv
 
 
 def ensure_dir(dir_name):
@@ -19,7 +20,7 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def download_txt(book_response, book_title, directory="./books/"):
+def download_txt(book_response, book_title, directory):
     sanitized_book_title = sanitize_filename(book_title)
     book_path = os.path.join(directory, sanitized_book_title)
     with open(book_path, "wb") as file:
@@ -27,8 +28,11 @@ def download_txt(book_response, book_title, directory="./books/"):
 
 
 def main():
-    ensure_dir("./images")
-    ensure_dir("./books")
+    load_dotenv()
+    images_directory = os.getenv("DIRECTORY_FOR_IMAGES")
+    books_directory = os.getenv("DIRECTORY_FOR_BOOKS")
+    ensure_dir(images_directory)
+    ensure_dir(books_directory)
 
     image_url = "https://dvmn.org/filer/canonical/1542890876/16/"
     image_response = requests.get(image_url)
@@ -36,7 +40,7 @@ def main():
 
     download_image(
         image_response,
-        "./images/dvmn.svg",
+        f"{images_directory}dvmn.svg",
     )
 
     for book_id in range(1, 11):
@@ -63,6 +67,7 @@ def main():
             download_txt(
                 book_response,
                 f"{book_title}.txt",
+                books_directory,
             )
         except requests.HTTPError:
             pass
