c62ede63f096220dc1b3fbb7f404571e34e46f73
AlexandraPankratova <ablackcat@yandex.ru>
2021-04-20 12:32:47 +0300

    Add def check_for_redirect

--- main.py
+++ main.py
@@ -7,34 +7,49 @@ def ensure_dir(dir_name):
     os.makedirs(dir_name, exist_ok=True)
 
 
-def download_image(image_url, image_name):
-    response = requests.get(image_url)
-    response.raise_for_status()
-
+def download_image(image_response, image_name):
     with open(image_name, "wb") as file:
-        file.write(response.content)
+        file.write(image_response.content)
 
 
-def download_book(book_url, book_title):
-    response = requests.get(book_url, verify=False)
-
+def download_book(response, book_title):
     with open(book_title, "wb") as file:
         file.write(response.content)
+
+
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError
 
 
 def main():
     ensure_dir("./images")
     ensure_dir("./books")
+
+    image_url = "https://dvmn.org/filer/canonical/1542890876/16/"
+    image_response = requests.get(image_url)
+    image_response.raise_for_status()
+
     download_image(
-        "https://dvmn.org/filer/canonical/1542890876/16/",
+        image_response,
         "./images/dvmn.svg",
     )
 
     for book_id in range(32159, 32169):
-        download_book(
+        book_response = requests.get(
             f"https://tululu.org/txt.php?id={book_id}",
-            f"./books/book_{book_id}.txt",
+            verify=False,
         )
+        book_response.raise_for_status()
+
+        try:
+            check_for_redirect(book_response)
+            download_book(
+                book_response,
+                f"./books/book_{book_id}.txt",
+            )
+        except requests.HTTPError:
+            pass
 
 
 if __name__ == '__main__':
